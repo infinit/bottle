06b7596a74bff1b9ee9a20250cbf51d2db8201cf
Julien (mycure) Quintard <julien.quintard@docker.com>
2017-06-12 10:55:19 +0200

    Fix undefined variable 'f'.

--- bottle.py
+++ bottle.py
@@ -1065,7 +1065,7 @@ class Bottle(object):
             if isinstance(id, str)  and route.name == id or \
                callable(id) and route.callback == id:
                 return self.router.build(route.rule, **parameters)
-        raise Exception('Reverse not found for %s.' % f)
+        raise Exception('Reverse not found for %s.' % id)
 
     def debug(self):
         return DEBUG
